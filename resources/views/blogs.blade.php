0f9028a9360a3d65cf0f4eb4b1663e6caf87a9b6
obande-code <jamesobande2015@gmail.com>
2021-10-11 10:04:36 +0100

    no message

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -8,7 +8,7 @@
           <img src="{{asset('storage/uploads/' .$blog->image. '')}}" class="blog-image" alt="">
           <h4>{{$blog->title}}</h4>
           <h6>{{$blog->date}}</h6>
-          <pre class="blog-body">{{$blog->body}}</pre>
+          <!-- <pre class="blog-body">{{$blog->body}}</pre> -->
         </div>
         @endif
         @endforeach
