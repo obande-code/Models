721bf1c23542898430d419b5ee616320fbc45376
obande-code <jamesobande2015@gmail.com>
2021-10-13 15:37:23 -0400

    no message

--- resources/views/stripe.blade.php
+++ resources/views/stripe.blade.php
@@ -73,11 +73,11 @@
 $(function() {
   var $form = $(".require-validation");
   $('form.require-validation').bind('submit', function(e) {
-    var $form = $(".require-validation"),
-    inputSelector = ['input[type=email]', 'input[type=password]', 'input[type=text]', 'input[type=file]', 'textarea'].join(', '),
-    $inputs = $form.find('.required').find(inputSelector),
-    $errorMessage = $form.find('div.error'),
-    valid = true;
+   //  var $form = $(".require-validation"),
+   //  inputSelector = ['input[type=email]', 'input[type=password]', 'input[type=text]', 'input[type=file]', 'textarea'].join(', '),
+   //  $inputs = $form.find('.required').find(inputSelector),
+   //  $errorMessage = $form.find('div.error'),
+   //  valid = true;
     $errorMessage.addClass('hide');
     $('.has-error').removeClass('has-error');
     $inputs.each(function(i, el) {
