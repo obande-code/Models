2dfb39207b779b63433dddf33fde06292ae47d42
obande-code <jamesobande2015@gmail.com>
2021-10-31 16:20:16 +0800

    no message

--- resources/views/editprofile.blade.php
+++ resources/views/editprofile.blade.php
@@ -9,11 +9,11 @@
                     <form method="POST" action="{{ route('saveprofile') }}" enctype="multipart/form-data">
                         @csrf
                         <a class="back_login" href="/userprofile">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            <!-- <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                 class="bi bi-arrow-left" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd"
                                     d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
-                            </svg>
+                            </svg> -->
                         </a>
                         <div class="form-group row justify-content-center">
                             <h5><span class="font-weight-bold">Edit Profile</span></h5>
