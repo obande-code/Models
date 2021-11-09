02ceae617eff868187e18ce8f2cb2f7ff7ca1f94
obande-code <jamesobande2015@gmail.com>
2021-11-10 04:18:45 +0800

    no message

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -19,7 +19,7 @@
         <div class="p-2 d-flex flex-column border rounded blog-block">
           <img src="{{asset('storage/uploads/' .$blog->image. '')}}" class="blog-image" alt="">
           <h4>{{$blog->title}}</h4>
-          <h6>{{$blog->date}}</h6>
+          <!-- <h6>{{$blog->date}}</h6> -->
           <p class="blog-body">{{$blog->body}}</p>
         </div>
         @endif
