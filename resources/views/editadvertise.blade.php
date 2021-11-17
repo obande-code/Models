fbfd807b368ba6c6f77ab5ef7a39b188d3afbd4c
obande-code <jamesobande2015@gmail.com>
2021-11-17 17:19:03 +0800

    no message

--- resources/views/editadvertise.blade.php
+++ resources/views/editadvertise.blade.php
@@ -19,9 +19,9 @@
                         required name="id" value="{{$post[0]->id}}">
                 </div>
                 <div class="form-group">
-                    <label for="description" class="font-weight-bold mb-0">Description</label>
+                    <!-- <label for="description" class="font-weight-bold mb-0">Description</label>
                     <input type="text" class="form-control signup_input" id="description" placeholder="Type here"
-                        required name="description" value="{{$post[0]->description}}" autofocus>
+                        required name="description" value="{{$post[0]->description}}" autofocus> -->
                 </div>
                 <button type="submit" class="btn btn-danger login_button fixed-bottom btn_addpost w-100">Upload</button>
             </form>
