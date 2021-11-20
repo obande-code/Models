39d64f0e57d674542d90a606978e49f7126a589d
obande-code <jamesobande2015@gmail.com>
2021-11-20 10:03:34 +0800

    no message

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -4,12 +4,12 @@
     <div class="d-flex flex-column">
         @foreach($blogs as $blog)
         @if($loop->index%2 == 0)
-        <!-- <div class="p-2 d-flex flex-column border rounded blog-block">
+        <div class="p-2 d-flex flex-column border rounded blog-block">
           <img src="{{asset('storage/uploads/' .$blog->image. '')}}" class="blog-image" alt="">
           <h4>{{$blog->title}}</h4>
           <h6>{{$blog->date}}</h6>
           <p class="blog-body">{{$blog->body}}</p>
-        </div> -->
+        </div>
         @endif
         @endforeach
     </div>
