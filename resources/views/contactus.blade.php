70a7733f5d99d270d9c5824c6e1622f0b46f806b
obande-code <jamesobande2015@gmail.com>
2021-11-25 08:17:04 +0800

    no message

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -32,9 +32,9 @@
         </div>
         <div class="form-group row">
           <label for="inputPassword3" class="col-sm-2 col-form-label">Email</label>
-          <!-- <div class="col-sm-10">
+          <div class="col-sm-10">
             <input type="password" class="form-control" id="inputPassword3">
-          </div> -->
+          </div>
         </div>
         <div class="form-group row">
           <label for="inputPassword3" class="col-sm-2 col-form-label">Message</label>
