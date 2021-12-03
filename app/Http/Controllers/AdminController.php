a323d689b64585835873aba15378d6fa775da054
obande-code <jamesobande2015@gmail.com>
2021-12-03 23:07:46 +0800

    no message

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -64,7 +64,7 @@ class AdminController extends Controller
     }
     public function newblog()
     {
-        return view('newblog');
+        // return view('newblog');
     }
     public function newfaq()
     {
